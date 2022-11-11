9eebd6477fc45fa86e4e7a526f9e5158b0cf2bab
AhmedSamy <93329018+ahsz10@users.noreply.github.com>
2022-11-11 16:49:09 +0200

    commit

--- backend/app/Http/Controllers/Api/AdminController.php
+++ backend/app/Http/Controllers/Api/AdminController.php
@@ -11,109 +11,123 @@ class AdminController extends Controller
 {
     public function view_category(){
         $data= Category::all();
-        // dd
         return $data;
-        // return "we are in view_category";
     }
 
     public function add_category(Request $request){
-        // $data = new category;
-        // $data->category_name = $request->category;
-        // $data->save();
-        // return redirect()->back()->with('message','New category added Successfully');
-        return "we are in add_category ";
-
+        $data = new category;
+        $data->category_name = $request->category;
+        $data->save();
+        // return "we are in add_category ";
+        // return 'done + $data';
     }
 
     public function destroy_category($categoryId){
-        // $single = Category::findOrFail($categoryId);
-        // $single -> delete();
-        // // Storage::delete($singlePost->image);
-        // return redirect()->back();
-        return "we are in destroy_category and id sent is $categoryId";
+        $data = Category::findOrFail($categoryId);
+        $data -> delete();
+        // return "we are in destroy_category and id sent is $categoryId";
+        // return 'done + $data';
     }
 
     public function view_product(){
-        // $data= Category::all();
-        // // dd($data);
-        // return view('admin.product',['category' => $data]);
-        return " we are in view all products";
+        $data= Product::all();
+        // return " we are in view all products";
+        return $data;
     }
 
     public function add_product(Request $request){//Request $request
-        // $product = new product;
-        // $product->title = $request->title;
-        // $product->description = $request->description;
-        // $product->price = $request->price;
-        // $product->quantity = $request->quantity;
-        // $product->category = $request->category;
+        $product = new product;
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->category = $request->category;
 
-        // $image = $request->file('image');
-        // // $image = $request->image;
-        // $imagename = time().'.'.$image->getClientOriginalExtension();
-        // $request->image->move('product' , $imagename);
-        // // echo Storage::putFile('product', $request->file('image'));
-        // $product->image = $imagename;
+        $image = $request->file('image');
+        // $image = $request->image;
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product' , $imagename);
+        // echo Storage::putFile('product', $request->file('image'));
+        $product->image = $imagename;
 
-        // $product->save();
+        $product->save();
         // return redirect()->back()->with('message','New Product added Successfully');
 
+        return $product;
         return " we are in add new products";
 
     }
 
     public function destroy_product($productId){
-        // $product = Product::findOrFail($productId);
-        // $product-> delete();
-        // return redirect()->back()->with('message','Product deleted Successfully');
-        return "we are in destroy_product and id sent is $productId";
+        $product = Product::findOrFail($productId);
+        $product-> delete();
+        // return "we are in destroy_product and id sent is $productId";
     }
 
 
-    public function update_product(Request $request,$productId){
-        // $product = Product::findOrFail($productId);
-        // $product->title = $request->title;
-        // $product->description = $request->description;
-        // $product->price = $request->price;
-        // $product->quantity = $request->quantity;
-        // $product->category = $request->category;
-        // $image = $request->file('image');
-        // if($image){
-        //     // $image = $request->image;
-        //     $imagename = time().'.'.$image->getClientOriginalExtension();
-        //     $request->image->move('product' , $imagename);
-        //     // echo Storage::putFile('product', $request->file('image'));
-        //     $product->image = $imagename;
-        // }
-        // $product->save();
+    public function update_product(Request $request){
+        $product = Product::findOrFail($request->id);
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->category = $request->category;
+        $image = $request->file('image');
+        if($image){
+            $imagename = time().'.'.$image->getClientOriginalExtension();
+            $request->image->move('product' , $imagename);
+            $product->image = $imagename;
+        }
+        $result=$product->save();
+        if($result){
+            return ["result" => "Updated"];
+        }else{
+            return
+            ["result"=> "Not Updated"];
+        }
 
-        return "we are in update_product and id sent is $productId";
+        // return "we are in update_product and id sent is $productId";
+        // return $product;
     }
 
 
 
-    public function searchByProductName($productName){ //$productName
-        // dd($request->pname);
+    public function searchByProductName($searchname){
         // $product = Product::findOrFail( $request->pname);
-        // $searchname = $request->pname;
-        // $product = Product::where('title','LIKE', "%$searchname%")->get();
+        // $searchname = $request->proudctname;
+        $product = Product::where('title','LIKE', "%$searchname%")->get();
+        //return "we are in searchByProductName and we want to search with  $productName";
+        return $product;
 
-        // // dd($product);
-        // // dd($product->title, $product->description);
-        // return view('admin.searchByProductName', ['product' => $product]);
-        return "we are in searchByProductName and we want to search with  $productName";//$productName
     }
 
     public function view_order(){
         // $order= Product::all();
         $orders= Product::all();
         return $orders;
-        return "we are in view all orders";
+        // return "we are in view all orders";
     }
 
-    public function search_order($ordername){
+    public function search_order($searchname){
         // $searchname = $request->pname;
-        // $order = Product::where('title','LIKE', "%$searchname%")->get();
-        return "we are in search_order and we want to search with  $ordername";//$productName
+        $order = Product::where('title','LIKE', "%$searchname%")->get();
+        // $order = Product::->where(['title'=>$searchname]);
+        // return "we are in search_order and we want to search with  $ordername";//$productName
+        return $order;
+    }
+
+    public function update_order(Request $request){
+        $order = Product::findOrFail($request->id);
+        $result = $order->update([
+            "price" => $request->status
+        ]);
+
+        if($result){
+            return ["result" => "Updated"];
+        }else{
+            return
+            ["result"=> "Not Updated"];
+        }
+        // return $order;
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -38,12 +38,13 @@ Route::delete('/delete_category/{id}', [AdminController::class, 'destroy_categor
 Route::get('/view_all_product', [AdminController::class,'view_product']);
 Route::post('/add_new_product', [AdminController::class,'add_product']);
 Route::delete('/delete_product/{id}', [AdminController::class, 'destroy_product']);
-Route::put('/update_product/{id}',[AdminController::class, 'update_product']);
+Route::put('/update_product',[AdminController::class, 'update_product']);
 Route::get('/searchByProductName/{name}', [AdminController::class,'searchByProductName']);
 
 // Order Api routes for Admin
 Route::get('/view_all_order', [AdminController::class,'view_order']);
 Route::get('/search_for_order/{name}', [AdminController::class,'search_order']);
+Route::put('/update_order',[AdminController::class, 'update_order']);
 
 
 
@@ -55,11 +56,12 @@ Route::get('/search_for_order/{name}', [AdminController::class,'search_order']);
 //  get       http://127.0.0.1:8000/api/view_all_product
 //  post      http://127.0.0.1:8000/api/add_new_product
 //  delete    http://127.0.0.1:8000/api/delete_product/{id}
-//  put       http://127.0.0.1:8000/api/update_product/{id}
+//  put       http://127.0.0.1:8000/api/update_product
 //  get       http://127.0.0.1:8000/api/searchByProductName/{name}
 
 //  get       http://127.0.0.1:8000/api/view_all_order
 //  get       http://127.0.0.1:8000/api/search_for_order/{name}
+//  put       http://127.0.0.1:8000/api/update_order
 
 
 
